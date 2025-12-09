1619522a93efb6e645322dcc31375ce3c677df8c
Piyush Sacher <piyush.sacher@chicmicstudios.in>
2025-12-09 18:01:08 +0530

    some bug fixed

--- server.js
+++ server.js
@@ -9,21 +9,17 @@ const brcypt = require("bcrypt")
 const PORT = process.env.PORT || 3000;
 const dataDir = path.join(__dirname, "data");
 
-// Make sure data folder exists
+
 if (!fs.existsSync(dataDir)) fs.mkdirSync(dataDir);
 
 const filePath = path.join(dataDir, "userdata.json");
 
-// POST /register
 app.post("/register", (req, res) => {
     const { username, email, password } = req.body;
 
     if (!username || !email || !password) {
         return res.status(400).json({ message: "username, email, and password required" });
     }
-
-
-    // Read current data
     let data = { users: [] };
     if (fs.existsSync(filePath)) {
         const raw = fs.readFileSync(filePath, "utf8");
@@ -31,18 +27,18 @@ app.post("/register", (req, res) => {
             try {
                 data = JSON.parse(raw);
             } catch (err) {
-                // If JSON is invalid, reset to empty
+                
                 data = { users: [] };
             }
         }
     }
 
-    // Check if email already exists
+    
     const exists = data.users.find(u => u.email === email);
     if (exists) {
         return res.status(400).json({ message: "Email already exists" });
     }
-    // Create new user
+   
     const nextId = data.users.length ? data.users[data.users.length - 1].id + 1 : 1;
     const sessionId = Date.now();
     const otp = Math.floor(100000 + Math.random() * 900000);
